969e191f975a559685e170f866923f50ae7e8955
davidkingham333 <david.kingham333@proton.me>
2025-09-17 22:19:22 +0200

    Update bytes_to_hex.py
    BytesToHexUpdated

--- bytes_to_hex.py
+++ bytes_to_hex.py
@@ -4,7 +4,7 @@ data2 = bytes([0b10101010, 0b11001100, 0b11110000])
 def binary_to_hex(data):
     return data.hex()
 
-# Funkce pro provedení XOR operace mezi dvìma binárními daty.
+# Funkce pro provedenÃ­ XOR operace mezi dvÃ¬ma binÃ¡rnÃ­mi daty.
 def xor_binary_data(data1, data2):
     return bytes(a ^ b for a,b in zip(data1, data2))
 
@@ -14,7 +14,7 @@ hex_data2 = binary_to_hex(data2)
 print(f"{hex_data1}")
 print("f{hex_data2}")
 
-# Provést XOR operaci
+# ProvÃ©st XOR operaci.
 result_data = xor_binary_data(data1, data2)
 
 hex_result = binary_to_hex(result_data)
