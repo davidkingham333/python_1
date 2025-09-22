2c2055852c7ad1c32ee4ed23c967a7710b055bfb
DavidK <david.kingham333@proton.me>
2025-09-22 08:56:32 +0200

    added new_file.py

---
+++ module6.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2,):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        res = nums1 + nums2
+        sorted_res = sorted(res)
+        n = len(sorted_res)
+        if n%2==1:
+            ans = sorted_res[n//2]
+        else:
+            ans = (sorted_res[n//2-1] + sorted_res[n//2])/2.0
+        return ans
+    
+
+sol = Solution()
+print(sol.findMedianSortedArrays([1,3], [2]))
+print(sol.findMedianSortedArrays([1,2], [3,4]))
